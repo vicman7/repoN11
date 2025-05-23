e6a012b34fdb8c148e68f692ad9f6f0e5b6905ad
vicman7 <tapiasv07@gmail.com>
2025-05-22 21:09:33 -0500

    first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="repoN11",                                            # Nombre del paquete
+    version="0.1.0",                                           # Versión inicial
+    packages=find_packages(),                                  # Paquetes a incluir
+    description="Un paquete pip simple de saludo",             # Breve descripción
+    author="Victor Manuel",                                    # Tu nombre
+    author_email="correodeprueba@estenoesmicorreo.com",        # Tu correo electrónico
+    url="https://github.com/vicman7/repoN11",                  # URL del proyecto
+)
